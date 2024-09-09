b840975592fc145511123b950e63a3c00d08bbf9
Kittisak Pharaphan <kungpotato@users.noreply.github.com>
2024-09-09 10:42:08 +0700

    ok

--- lib/core/widgets/widgets.dart
+++ lib/core/widgets/widgets.dart
@@ -2,5 +2,6 @@ export './custom_appbar.dart';
 export './image_slide.dart';
 export './kp_image.dart';
 export './kp_tab.dart';
+export './kp_video.dart';
 export './layout.dart';
 export './menu_scroll.dart';
