308946b9ef900da6e47ed636f8bb05bee4221934
Kittisak Pharaphan <kungpotato@users.noreply.github.com>
2024-09-14 08:04:03 +0700

    ok

---
+++ lib/core/design/molecules/kp_search_input.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class SearchWidget extends StatelessWidget {
+  const SearchWidget({this.onTap, this.colors, super.key});
+
+  final void Function()? onTap;
+  final List<Color>? colors;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(color: theme.primaryColor),
+          gradient: colors != null
+              ? LinearGradient(
+                  colors: colors!,
+                )
+              : null,
+        ),
+        width: double.maxFinite,
+        child: TextFormField(
+          enabled: false,
+          decoration: InputDecoration(
+            hintText: 'ค้นหา',
+            contentPadding:
+                const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
+            hintStyle: theme.textTheme.labelMedium
+                ?.copyWith(color: theme.primaryColor),
+            fillColor: Colors.transparent,
+            filled: true,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide.none,
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide.none,
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide.none,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
